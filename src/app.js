7f46b6e7b3af73d343671548692f521c507909a5
Fareed <fareed.ch7869@gmail.com>
2024-08-20 02:02:54 +0500

    setup express and make utils files

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"})) //limiting json data to handle server crash //its form data
+app.use(express.urlencoded({extended: true, limit: "16kb"})) // for url data ( + % )
+app.use(express.static("public")) // files like pdf images that yu want to store in server , public is folder name
+app.use(cookieParser()) // from our server access users browser cookies, secure way to handle cookies
+
+export { app }
