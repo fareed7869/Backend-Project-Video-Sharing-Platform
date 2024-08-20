3b87eb8bfc7d5cdd6e718a971f98be8b4e97b52e
Fareed <fareed.ch7869@gmail.com>
2024-08-20 22:20:13 +0500

    add setup and functionalities for router and controller

--- src/app.js
+++ src/app.js
@@ -15,4 +15,13 @@ app.use(express.urlencoded({extended: true, limit: "16kb"})) // for url data ( +
 app.use(express.static("public")) // files like pdf images that yu want to store in server , public is folder name
 app.use(cookieParser()) // from our server access users browser cookies, secure way to handle cookies
 
+//routes import
+import userRouter from './routes/userRoutes.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
+
 export { app }
